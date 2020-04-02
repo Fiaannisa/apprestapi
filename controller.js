f70814cdd69aba63fd3de447f1b4cfea7d80e866
fiaannisa <annisafia06@gmail.com>
2020-04-02 10:46:46 +0700

    berhasil menampilkan semua data mahasiswa

--- controller.js
+++ controller.js
@@ -5,4 +5,16 @@ var connection = require('./koneksi');
 
 exports.index = function(req,res){
     response.ok("Aplikasi REST API ku berjalan!",res)
+};
+
+//menampilkan semua data mahasiswaa
+exports.tampilsemuamahasiswa = function(req,res){
+    connection.query('select * FROM mahasiswa', function(error, rows, fileds){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+        
+    })
 }
